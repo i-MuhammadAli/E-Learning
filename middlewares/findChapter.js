9c18e21ed38adb6845b8446e1f96c5ace729d6cb
Muhammad Ali <muhammad_ali@nextpak.org>
2025-02-08 15:48:08 +0500

    Add find chapter middleware

---
+++ middlewares/findChapter.js
@@ -0,0 +1,25 @@
+const CHAPTER = require("../models/chapterModal");
+
+const findChapter = async (req, res, next) => {
+  try {
+    const chapterId = req.params.id;
+    const chapter = await CHAPTER.findById(chapterId);
+
+    if (!chapter) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Chapter not found" });
+    }
+
+    req.chapter = chapter;
+    next();
+  } catch (error) {
+    console.error("Finding chapter error:", error);
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+module.exports = findChapter;
